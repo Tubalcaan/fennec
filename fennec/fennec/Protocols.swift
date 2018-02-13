ae94a95e3bac3d1604142f86c626d79e972a03a8
eliksir <seb.knopf@gmail.com>
2018-02-13 20:23:04 +0100

    fennec v2

--- fennec/fennec/Protocols.swift
+++ fennec/fennec/Protocols.swift
@@ -12,9 +12,15 @@ import Foundation
 // Component
 //////////////////////
 public protocol Updatable {
-    mutating func update()
+    mutating func update(deltaTime seconds: TimeInterval)
 }
 public protocol Component: Updatable {
+}
+
+extension Component {
+    var componentType: ComponentType {
+        return String(describing: type(of: self))
+    }
 }
 
 //////////////////////
@@ -30,35 +36,5 @@ public protocol Composable {
     func removeComponent<T>(_ componentType: T.Type) where T : Component
 }
 
-public protocol Entity: Unique, Composable, Updatable {
-}
-public extension Entity {
-    public func update() {
-        if let allKeys = System.shared.components[id]?.keys {
-            let keys = Array(allKeys)
-            for i in 0..<keys.count {
-                let key = keys[i]
-                if var component = System.shared.components[id]?[key] {
-                    component.update()
-                    if Mirror(reflecting: component).displayStyle != Mirror.DisplayStyle.class {
-                        System.shared.components[id]?[key] = component
-                    }
-                }
-            }
-        }
-    }
-}
-extension Entity {
-    public func addComponent(_ component: Component) {
-        if System.shared.components[id] == nil {
-            System.shared.components[id] = [:]
-        }
-        System.shared.components[id]![String(describing: type(of: component))] = component
-    }
-    public func component<T>(_ componentType: T.Type) -> T? where T : Component {
-        return System.shared.components[id]?[String(describing: componentType)] as? T
-    }
-    public func removeComponent<T>(_ componentType: T.Type) where T : Component {
-        System.shared.components[id]?[String(describing: componentType)] = nil
-    }
+public protocol Entity: Unique, Composable {
 }
--- fennec/fennec/System.swift
+++ fennec/fennec/System.swift
@@ -10,15 +10,39 @@ import Foundation
 
 // TODO: remove entity from group
 
+public extension Entity {
+    public func addComponent(_ aComponent: Component) {
+        if Engine.shared.allComponents[aComponent.componentType] == nil {
+            Engine.shared.allComponents[aComponent.componentType] = [:]
+        }
+        Engine.shared.allComponents[aComponent.componentType]![id] = aComponent
+    }
+    public func component<T>(_ componentType: T.Type) -> T? where T : Component {
+        return Engine.shared.allComponents[String(describing: componentType)]?[id] as? T
+    }
+    public func removeComponent<T>(_ componentType: T.Type) where T : Component {
+        Engine.shared.allComponents[String(describing: componentType)]?[id] = nil
+    }
+    subscript<T>(_ componentType: T.Type) -> T? where T : Component {
+        return component(componentType)
+    }
+}
+
+
 public typealias ComponentType = String
-public struct System: Updatable {
-    public static var shared = System()
-    private var entities: [Entity] = []
-    var components: [UUID:[ComponentType:Component]] = [:]
+public protocol System: Unique {
+    func update(deltaTime seconds: TimeInterval)
+}
+
+public struct Engine {
+    public static var shared = Engine()
+    private var allEntities: [UUID:Entity] = [:]
+    fileprivate var allComponents: [ComponentType:[UUID:Component]] = [:]
     private var groups: [String:[Entity]] = [:]
+    private var systems: [UUID:System] = [:]
     
-    public mutating func addEntity(_ entity: Entity, toGroup group: String = "") {
-        entities.append(entity)
+    public mutating func addEntity(_ entity: Entity, inGroup group: String = "") {
+        allEntities[entity.id] = entity
         if groups[""] == nil {
             groups[""] = []
         }
@@ -26,31 +50,37 @@ public struct System: Updatable {
             groups[group] = []
         }
         groups[""]?.append(entity)
-        groups[group]?.append(entity)
+        if group != "" {
+            groups[group]?.append(entity)
+        }
     }
     
     public mutating func removeEntity(_ entity: Entity) {
-        if let index = entities.index(where: { (ent: Entity) -> Bool in
-            ent.id == entity.id
-        }) {
-            entities.remove(at: index)
-            components[entity.id] = nil
-        }
-    }
-    
-    public func update() {
-        entities.forEach { entity in
-            entity.update()
-        }
-    }
-    
-    public func update(entitiesInGroup group: String = "") {
-        if group == "" {
-            update()
-        } else {
-            groups[group]?.forEach { entity in
-                entity.update()
+        allEntities[entity.id] = nil
+        // TODO
+        // remove entity from "" group and its group
+        groups.enumerated().forEach { (offset: Int, element: (key: String, value: [Entity])) in
+            if let index = groups[element.key]?.index(where: { ent -> Bool in
+                entity.id == ent.id
+            }) {
+                groups[element.key]?.remove(at: index)
             }
+        }
+        // remove all components of the entity
+        var comps: [Component] = []
+        allComponents.enumerated().forEach { (offset: Int, element: (key: ComponentType, value: [UUID:Component])) in
+            if let component = allComponents[element.key]?[entity.id] {
+                comps.append(component)
+            }
+        }
+        comps.forEach { comp in
+            allComponents[comp.componentType]?[entity.id] = nil
+        }
+    }
+    
+    public func update(deltaTime seconds: TimeInterval) {
+        systems.enumerated().forEach { (offset: Int, element: (key: UUID, value: System)) in
+            element.value.update(deltaTime: seconds)
         }
     }
     
@@ -58,17 +88,31 @@ public struct System: Updatable {
         return groups[group] ?? []
     }
     
-    public func execute(_ block: (Entity)->(), onGroup group: String = "") {
-        let selectedEntities: [Entity]
+    public mutating func addSystem(_ system: System) {
+        systems[system.id] = system
+    }
+    
+    public mutating func removeSystem(_ system: System) {
+        systems[system.id] = nil
+    }
+    
+    public func entity(_ id: UUID) -> Entity? {
+        return allEntities[id]
+    }
+    
+    public func entities<T>(with componentType: T.Type) -> [Entity] {
+        let comps = allComponents[String(describing: componentType)]?.flatMap({ (tuple: (key: UUID, value: Component)) -> Entity? in
+            return allEntities[tuple.key]
+        }) ?? []
         
-        if group == "" {
-            selectedEntities = entities
-        } else {
-            selectedEntities = groups[group] ?? []
-        }
+        return comps
+    }
+    
+    public func components<T>(_ componentType: T.Type) -> [T] where T : Component {
+        let comps = allComponents[String(describing: componentType)]?.map({ (tuple: (key: UUID, value: Component)) -> Component in
+            return tuple.value
+        }) ?? []
         
-        selectedEntities.forEach { entity in
-            block(entity)
-        }
+        return comps as! [T]
     }
 }
